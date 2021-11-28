168146df4a7851d8ac14b2534f92069c2f2083ce
zhouhanseng <z308114274@gmail.com>
2021-11-28 12:00:40 +0800

    Init

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://dimension.github.io`,
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-use-query-params",
+  ],
+  pathPrefix: "/DAO-Interface",
+};
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+// markup
+const IndexPage = () => {
+  return (
+    <main>
+      <p className="font-bold text-blue-500">123</p>
+    </main>
+  )
+}
+
+export default IndexPage
